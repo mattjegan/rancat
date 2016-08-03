8eae3653052ed27eec24591f351ffabbadcedc74
mattjegan <matthewj.egan@hotmail.com>
2016-08-03 22:21:51 +1000

    Fixed up setup.py

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from distutils.core import setup
 
 setup(
     name='RanCat',
-    version='0.1',
+    version='0.0.1',
     description='A random string generator',
     author='Matthew Egan',
     url='https://github.com/mattjegan/rancat',
-    py_module=['rancat']
+    packages=['rancat']
 )
