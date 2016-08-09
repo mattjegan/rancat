de6da004e8b86b090fbecb7963c2d0961da7c3ab
mattjegan <matthewj.egan@hotmail.com>
2016-08-09 18:09:15 +1000

    Added lazy load using read_size

--- rancat/RanCat.py
+++ rancat/RanCat.py
@@ -1,7 +1,7 @@
 import random
 
 class RanCat:
-    def __init__(self, seed=None, unique=False):
+    def __init__(self, seed=None, unique=False, read_size=1000):
         from collections import OrderedDict
         self.files = OrderedDict()
 
@@ -10,9 +10,10 @@ class RanCat:
         random.seed(self.seed)
 
         self._conversion = self._default_conversion
-        self._unique = unique
+        self._unique = bool(unique)
         self._total_combinations = 0
         self._seen_map = {}
+        self._read_size = int(read_size)
 
     def __iter__(self):
         return self
@@ -21,7 +22,8 @@ class RanCat:
         return self.next()
 
     def next(self):
-        self._open_all()
+        if (len(self._seen_map.keys()) < self._total_combinations and len(self._seen_map.keys()) > self._total_combinations // 2) or len(self._seen_map.keys()) == 0:
+            self._refresh_all(self._read_size)
 
         if len(self._seen_map.keys()) == self._total_combinations:
             raise StopIteration('Exhausted combinations')
@@ -30,8 +32,8 @@ class RanCat:
         seen = False
         while not seen:
             result_string = ''
-            for f in self.files.values():    
-                choice = random.choice(f)
+            for file_tuple in self.files.values():    
+                choice = random.choice(file_tuple[1])
                 result_string += self._conversion(choice) + '_'
             result_string = result_string[:-1]
 
@@ -45,14 +47,7 @@ class RanCat:
         return result_string
 
     def load(self, filepath):
-        """
-        TODO: Make this a lazy load
-        """
-        self.files[filepath] = [line for line in open(filepath, 'r')]
-        if self._total_combinations != 0:
-            self._total_combinations *= len(self.files[filepath])
-        else:
-            self._total_combinations = len(self.files[filepath])
+        self.files[filepath] = [open(filepath, 'r'), [], True] # (file_obj, current_lines, open)
         return self
 
     def _open_all(self):
@@ -62,6 +57,27 @@ class RanCat:
         TODO: Implement when doing lazy loading
         """
         return
+
+    def _refresh_all(self, n):
+        """
+        Reads in the next n lines from the files
+        """
+        self._total_combinations = 0
+        for filepath in self.files:
+            if self.files[filepath][2]:
+                for i in range(0, n):
+                    line = self.files[filepath][0].readline()
+                    if not line:
+                        self.files[filepath][0].close()
+                        self.files[filepath][2] = False
+                        break
+                    self.files[filepath][1].append(line)
+
+            # Recalculate _total_combinations
+            if self._total_combinations == 0:
+                self._total_combinations = len(self.files[filepath][1])
+            else:
+                self._total_combinations *= len(self.files[filepath][1])
 
     def _default_conversion(self, phrase):
         """
@@ -83,5 +99,9 @@ class RanCat:
 
     def set_unique(self, boolean):
         self._unique = bool(boolean)
+        return self
+
+    def set_read_size(self, read_size):
+        self._unique = int(read_size)
         return self
         
