580a81438fbbcd482ed8824dc0d556c910fd65d3
mattjegan <matthewj.egan@hotmail.com>
2016-08-16 18:46:57 +1000

    Added requirements.txt, updated version num for next release and updated gitignore

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='RanCat',
-    version='0.0.3',
+    version='1.0.0',
     description='A random string generator',
     author='Matthew Egan',
     url='https://github.com/mattjegan/rancat',
